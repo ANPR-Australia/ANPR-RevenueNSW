7bb0f722011cccbe6cf22bbd7a4acafbe3b53ae9
Sara Falamaki <sarafalamaki-github@falamaki.id.au>
2020-03-23 20:04:55 +1100

    added stubs for experiment functions

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 [DEFAULT]
 labeled_data_dir =/Users/sara/work/ANPR-RevenueNSW/data/mobile_speed_camera_files/
 labeled_data_output = /Users/sara/work/ANPR-RevenueNSW/data/labeled_data.csv
-training_data_dir = ""
-test_data_dir = ""
+training_data_dir = /Users/sara/work/ANPR-RevenueNSW/data/training
+test_data_dir = /Users/sara/work/ANPR-RevenueNSW/data/test
+
---
+++ src/run_experiments.py
@@ -0,0 +1,29 @@
+import configparser
+import os
+import yaml
+import glob
+
+
+def train_labeled_images():
+    pass
+
+
+def train_fonts():
+    pass
+
+
+def test_untrained_uncaliberated_system():
+    pass
+
+
+def test_untrained_caliberated_system():
+    pass
+
+
+def test_trained_system_no_fonts():
+    pass
+
+
+def test_trained_system_with_fonts():
+    pass
+
