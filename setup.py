4ee1421795ec07c3a82b406f2cfe4abd67631d32
Sara Falamaki <sarafalamaki-github@falamaki.id.au>
2020-03-23 16:52:16 +1100

    added pyaml dep create_labeled_data generates a dict that can be used elsewhere

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     install_requires = [
         'numpy',
         'openalpr'
+        'pyaml'
         ],
     extras_require = {
         "dev": [
--- src/create_labeled_data.py
+++ src/create_labeled_data.py
@@ -12,16 +12,24 @@ print(labeled_data_dir)
 labeled_data_output = config["DEFAULT"]["labeled_data_output"]
 out = open(labeled_data_output, "a+")
 
-files = [f for f in glob.glob(labeled_data_dir + "/*.yaml", recursive=False)]
-print(files)
+
+def create_labeled_data():
+    label_dict = {}
+    files = [f for f in glob.glob(labeled_data_dir + "/*.yaml", recursive=False)]
+    print(files)
 
 
-for f in files:
-    with open(f, 'r') as stream:
-        try:
-            contents = yaml.safe_load(stream)
-            out.write("%s,%s,%s\n" % (contents['image_file'], contents['region_code_gt'], contents['plate_number_gt']))
-        except yaml.YAMLError as exc:
-            print(exc)
+    for f in files:
+        with open(f, 'r') as stream:
+            try:
+                contents = yaml.safe_load(stream)
+                out.write("%s,%s,%s\n" % (contents['image_file'], contents['region_code_gt'], contents['plate_number_gt']))
+                label_dict[contents['image_file']] = (contents['region_code_gt'], contents['plate_number_gt'])
+            except yaml.YAMLError as exc:
+                print(exc)
 
-out.close()
+    out.close()
+    return label_dict
+
+if __name__ == "__main__":
+    print(create_labeled_data())
